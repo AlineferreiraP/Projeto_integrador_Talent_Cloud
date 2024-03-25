64b1bfc23fed60ab41adb5269619132f26d40d5b
Jane Ceilan de Araujo <janejca@gmail.com>
2024-03-25 18:11:52 -0300

    sprint 3

---
+++ teste.js
@@ -0,0 +1,11 @@
+// Função para alternar a exibição do dropdown
+function toggleDropdown() {
+  var dropdownContent = document.getElementById("dropdownContent");
+  dropdownContent.classList.toggle("show");
+}
+
+// Função para selecionar um gênero de livro
+function selectGenre(genre) {
+  // Simplesmente imprima o gênero selecionado por enquanto
+  console.log("Gênero selecionado: " + genre);
+}
